b91ee4f236a4afddc65c28487b619c0f7bf1f9d4
umermalik053 <um4273209@gmail.com>
2025-01-25 19:58:14 -0800

    done

--- helper/emailTranspoter.js
+++ helper/emailTranspoter.js
@@ -6,11 +6,22 @@ dotenv.config();
 
 const emailTransporter = nodemailer.createTransport({
     service: "gmail",
+
     auth: {
       user: process.env.TRANSPORTER_EMAIL,
       pass: process.env.TRANSPORTER_PASSWORD,
     },
   });
+
+// const emailTransporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST,
+//   port: 587,
+//   secure: false, // true for 465, false for other ports
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_USER_PASSWORD,
+//   },
+// })
 
   export default emailTransporter;
 
